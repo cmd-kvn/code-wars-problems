c333217d2dfae6737599f5ec541e40fb957290ca
cmd-kvn <kvn.m.wong@gmail.com>
2017-08-09 15:28:47 -0700

    passes reverseWords from reversed-words.js

---
+++ test/6kyu.test.js
@@ -0,0 +1,16 @@
+const { assert } = require('chai');
+
+const dir = '../6kyu/';
+const reverseWords = require(`${dir}reversed-words`);
+
+describe('6kyu katas', () => {
+    let tests, answers;
+
+    it.only('Passes "reverseWords"', () => {
+        tests = ['hello world!', 'yoda doesn\'t speak like this', 'foobar', 'kata editor', 'row row row your boat'];
+        answers = ['world! hello', 'this like speak doesn\'t yoda', 'foobar', 'editor kata', 'boat your row row row'];
+
+        tests.map((test, i) => assert.equal(reverseWords(test), answers[i]));
+    });
+
+});
