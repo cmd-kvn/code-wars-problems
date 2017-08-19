8260f97161421d3f0547c9aa9b4af034b2c91d2c
cmd-kvn <kvn.m.wong@gmail.com>
2017-08-18 20:35:56 -0700

    passes 'list' from format-a-string-of-names.js

--- test/5kyu.test.js
+++ test/5kyu.test.js
@@ -5,7 +5,7 @@ const orderWeights = require('../5kyu/weightForWeight');
 describe('5kyu katas', () => {
     let tests, answers; 
 
-    it.only('Passes "weight for weight"', () => {
+    it('Passes "weight for weight"', () => {
         tests = ['103 123 4444 99 2000', '2000 10003 1234000 44444444 9999 11 11 22 123'];
         answers = ['2000 103 123 4444 99', '11 11 2000 10003 22 123 1234000 44444444 9999'];
 
--- test/6kyu.test.js
+++ test/6kyu.test.js
@@ -2,6 +2,7 @@ const { assert } = require('chai');
 
 const dir = '../6kyu/';
 const reverseWords = require(`${dir}reversed-words`);
+const list = require(`${dir}format-a-string-of-names`);
 
 describe('6kyu katas', () => {
     let tests, answers;
@@ -11,6 +12,15 @@ describe('6kyu katas', () => {
         answers = ['world! hello', 'this like speak doesn\'t yoda', 'foobar', 'editor kata', 'boat your row row row'];
 
         tests.map((test, i) => assert.equal(reverseWords(test), answers[i]));
+    });
+
+    it('Passes "list"', () => {
+        tests = [[{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }, { name: 'Homer' }, { name: 'Marge' }],
+            [{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }], 
+            [{ name: 'Bart' }, { name: 'Lisa' }], [{ name: 'Bart' }], []];
+        answers = ['Bart, Lisa, Maggie, Homer & Marge', 'Bart, Lisa & Maggie', 'Bart & Lisa', 'Bart', ''];
+
+        tests.map((test, i) => assert.equal(list(test), answers[i]));
     });
 
 });
