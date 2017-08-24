b2ee459d4dbb46d1536c7f7f7907e0657e088901
cmd-kvn <kvn.m.wong@gmail.com>
2017-08-24 04:41:47 -0700

    passes alphabetPosition from replace-with-alphabet-position.js

--- test/6kyu.test.js
+++ test/6kyu.test.js
@@ -6,6 +6,7 @@ const list = require(`${dir}format-a-string-of-names`);
 const calculateHypotenuse = require(`${dir}calculate-hypotenuse-of-right-triangle`);
 const reverseWords = require(`${dir}reverse-words`);
 const { biggestNum, isNegOrPos, removeLeadZeros } = require(`${dir}biggest-number-with-strings`);
+const alphabetPosition = require(`${dir}replace-with-alphabet-position`);
 
 describe('6kyu katas', () => {
     let tests, answers;
@@ -71,6 +72,20 @@ describe('6kyu katas', () => {
         
             tests.map((test, i) => assert.equal(biggestNum(test[0], test[1]), answers[i]));        
         });
+    });
+
+    it('Passes "alphabetPostion"', () => {
+        tests = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'The sunset sets at twelve o\' clock.', 'The narwhal bacons at midnight.', 
+            '43@@)2<$', ' ^(w=v70*'];
+        answers = ['1', '2', '3', '4', '5', '6', '7', '8','9', '10', '11', '12', '13',  
+            '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', 
+            '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11',
+            '20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20', 
+            '', '23 22'];
+        
+        tests.map((test, i) => assert.equal(alphabetPosition(test), answers[i]));
     });
 
 });
